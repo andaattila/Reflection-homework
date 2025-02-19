0f3c967245d9e1e449c8e26d9fc95008fd7cab9e
andaattila <attila.anda@gmail.com>
2025-02-19 22:54:59 +0100

    My FuzzBizz homework

--- FizzBuzz/src/FizzBuzz.java
+++ FizzBuzz/src/FizzBuzz.java
@@ -3,11 +3,23 @@ import java.util.Scanner;
 public class FizzBuzz {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-int number= scanner.nextInt();
-if (number<1){
-    System.out.println("Please enter a valid number!");
-    int number = scanner.nextInt();
-}
+        System.out.println("Please enter a number and I will show you what the Fizz Buzz is! ");
+        int n = scanner.nextInt();
+        if (n < 1) {
+            System.err.println("Please enter a number greater than 0!");
+        } else {
+            for (int i = 1; i <= n; i++) {
+                if (i % 3 == 0 && i % 5 != 0) {
+                    System.out.println("Fizz");
+                } else if (i % 5 == 0 && i % 3 != 0) {
+                    System.out.println("Buzz");
+                } else if (i % 5 == 0 && i % 3 == 0) {
+                    System.out.println("FizzBuzz");
+                } else {
+                    System.out.println(i);
+                }
+            }
+        }
 
     }
 }
