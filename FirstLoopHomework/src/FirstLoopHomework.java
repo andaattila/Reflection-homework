d88bdf94332fa1ecd9f664c34f655a56a603c952
andaattila <attila.anda@gmail.com>
2025-02-19 20:58:19 +0100

    My first loop homework is ready

--- FirstLoopHomework/src/FirstLoopHomework.java
+++ FirstLoopHomework/src/FirstLoopHomework.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
 
-public class Main {
+public class FirstLoopHomework {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Type a text and after a number:");
         String text = scanner.nextLine();
         int n = scanner.nextInt();
-        if (n < 0) {
-            System.err.println("Your number is smaller than zero!");
-        } else {
-            for (int i = 0; i < i; i++) {
+        if (n > 0) {
+            for (int i = 0; i < n; i++) {
                 System.out.println(text);
             }
+        } else {
+            System.err.println("Your number is smaller than zero!");
         }
+        System.exit(0);
     }
 }
---
+++ FizzBuzz/src/FizzBuzz.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+int number= scanner.nextInt();
+if (number<1){
+    System.out.println("Please enter a valid number!");
+    int number = scanner.nextInt();
+}
+
+    }
+}
