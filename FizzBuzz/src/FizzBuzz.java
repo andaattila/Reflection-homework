c11bdd0414cb1fc5548510d4993529992c8497c6
andaattila <attila.anda@gmail.com>
2025-02-20 16:32:06 +0100

    Modified FizzBuzz

--- FizzBuzz/src/FizzBuzz.java
+++ FizzBuzz/src/FizzBuzz.java
@@ -13,13 +13,12 @@ public class FizzBuzz {
                     System.out.println("Fizz");
                 } else if (i % 5 == 0 && i % 3 != 0) {
                     System.out.println("Buzz");
-                } else if (i % 5 == 0 && i % 3 == 0) {
+                } else if (i % 5 == 0) {
                     System.out.println("FizzBuzz");
                 } else {
                     System.out.println(i);
                 }
             }
         }
-
     }
 }
