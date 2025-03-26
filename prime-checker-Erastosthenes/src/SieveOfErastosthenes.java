51c60f244f1e7a0ba30fa6d994215f5f71062deb
andaattila <attila.anda@gmail.com>
2025-03-26 21:41:17 +0100

    V2 tip calculator

---
+++ prime-checker-Erastosthenes/src/SieveOfErastosthenes.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SieveOfErastosthenes {
+    public static void main(String[] args) {
+        boolean[] primes = new boolean[number+1];
+        for (int i = 0; i < primes.length ; i++) {
+            primes[i]=true;
+        }
+        Primes[0]=false;
+        Primes[1]=false;
+
+        if ()
+        for (int i = 0; i <Primes.length; i++) {
+
+
+        }
+
+        }
+        
+    }
+}
--- tip-calculator/src/TipCalculator.java
+++ tip-calculator/src/TipCalculator.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 
 public class TipCalculator {
     static Scanner scanner = new Scanner(System.in);
+    static int[] costs;
+    static double percentage;
 
     public static void main(String[] args) {
         System.out.println("Enter how many costs do you have:");
-        int[] costs = new int[scanner.nextInt()];
+        costs = new int[scanner.nextInt()];
         System.out.println("Enter the costs you have spent:");
         for (int i = 0; i < costs.length; i++) {
             costs[i] = scanner.nextInt();
         }
 
-        System.out.println("Enter how many percentage do you want to give: [0,...]");
-        double percentage = scanner.nextDouble();
+        System.out.println("Enter how many percentage do you want to give: ");
+        percentage = scanner.nextDouble();
 
         System.out.println("The sum of the cost is:");
         System.out.println(sumTheCosts(costs));
@@ -20,10 +22,11 @@ public class TipCalculator {
         System.out.println("The percentage you have given is:");
         System.out.println(checkThePercentage(percentage));
 
-        System.out.println("The tip needs to be:");
-        double tip = sumTheCosts(costs) * checkThePercentage(percentage);
-        System.out.println((int)tip);
+        System.out.println("The Tip needs to be:");
+        System.out.println(tipCalculator());
+
     }
+
     public static int sumTheCosts(int[] costs) {
         int sum = 0;
         for (int cost : costs) {
@@ -31,11 +34,18 @@ public class TipCalculator {
         }
         return sum;
     }
-    public static double checkThePercentage(double percentage) {
+
+    public static int checkThePercentage(double percentage) {
         double result = -1;
-        if (percentage <= 0.1 || percentage >= 0.8) {
-            return result;
-        } else return percentage;
+        if (percentage <= 10 || percentage >= 80) {
+            return (int) result;
+        } else return (int) percentage;
+    }
+
+    public static int tipCalculator() {
+
+        double tip = sumTheCosts(costs) * checkThePercentage(percentage) / 100;
+        return (int) tip;
     }
 }
 
